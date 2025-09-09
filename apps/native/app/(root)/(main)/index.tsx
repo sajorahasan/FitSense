dfcff034740013cb8f3cc66bba94d979f50b996c
Hasan Sajora <hasan.sajora@superwise.ai>
2025-09-09 11:59:59 +0530

    fix(app): update API import path in index.tsx
    - Changed the API import from `@my-better-t-app/backend/convex/_generated/api` to `@fitsense/backend/convex/_generated/api`.
    - Refactored text components for improved readability by removing unnecessary line breaks.
    
    These changes ensure the application correctly references the updated backend API and enhance the layout of the home route.

--- apps/native/app/(root)/(main)/index.tsx
+++ apps/native/app/(root)/(main)/index.tsx
@@ -1,5 +1,5 @@
 import { Ionicons } from "@expo/vector-icons";
-import { api } from "@my-better-t-app/backend/convex/_generated/api";
+import { api } from "@fitsense/backend/convex/_generated/api";
 import { useQuery } from "convex/react";
 import { Link } from "expo-router";
 import { Button, useTheme } from "heroui-native";
@@ -21,29 +21,17 @@ export default function HomeRoute() {
       }}
     >
       <View className="flex-1 justify-end">
-        <Text className="font-extrabold text-6xl text-foreground">
-          Welcome {userData.userMetaData.name}
-        </Text>
-        <Text className="text-muted-foreground text-xl">
-          {userData.userMetaData.email}
-        </Text>
+        <Text className="font-extrabold text-6xl text-foreground">Welcome {userData.userMetaData.name}</Text>
+        <Text className="text-muted-foreground text-xl">{userData.userMetaData.email}</Text>
         <Text className="text-muted-foreground text-xl">
           on {new Date(userData.userMetaData.createdAt).toDateString()}
         </Text>
       </View>
       <Link href={"/(root)/(main)/settings"} asChild>
-        <Button
-          className="rounded-full"
-          size={"lg"}
-          onPress={() => console.log("Button pressed")}
-        >
+        <Button className="rounded-full" size={"lg"} onPress={() => console.log("Button pressed")}>
           <Button.LabelContent>Go to settings</Button.LabelContent>
           <Button.EndContent>
-            <Ionicons
-              name="chevron-forward"
-              size={18}
-              color={colors.background}
-            />
+            <Ionicons name="chevron-forward" size={18} color={colors.background} />
           </Button.EndContent>
         </Button>
       </Link>
